e328d8b10b59ef43cd0e33998adf2af8e62c4e35
Jasper Rheinänder <jasper.rheinlaender@campus.tu-berlin.de>
2026-07-14 10:32:06 +0200

    Fixed Basket Control

--- Assets/Scripts/followMouseHorizontal.cs
+++ Assets/Scripts/followMouseHorizontal.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class followMouseHorizontal : MonoBehaviour
 {
+    private float minX;
+    private float maxX;
+
+    void Start()
+    {
+        minX = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + 0.6f;
+        maxX = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - 0.6f;
+    }
+
     void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        float x = Mathf.Clamp(mousePos.x, minX, maxX);
+
         transform.position = new Vector3(
-            mousePos.x,
+            x,
             transform.position.y,
             transform.position.z
         );
